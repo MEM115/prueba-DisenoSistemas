4e43823afdd86cb139b7c911f41caad3beb971ef
Matias Magazo <mem173115@gmail.com>
2017-04-01 10:59:13 -0300

    Agregar clases

---
+++ src/main/java/Class1.java
@@ -0,0 +1,14 @@
+
+public class Class1 {
+	public int Edad;
+	public String Nombre;
+	public int HacerAlgo(String value){
+		if(value != "")
+		{
+			return 1;
+		}
+		else{
+			return 0;
+		}
+	}
+}
